ab89562c617ca5d925726d1d5fd95d5da5e6ff4d
Zayez <zayezuno@protonmail.com>
2025-03-19 20:02:05 -0300

    Typing format validations of joi

--- server/src/helpers/response-helpers.ts
+++ server/src/helpers/response-helpers.ts
@@ -1,19 +1,20 @@
+import { ValidationErrorItem } from 'joi'
+
 /**
  * Formats joi validation error details into a Problem Details JSON response.
  * See: https://www.rfc-editor.org/rfc/rfc7807#section-3
  * @param {array} details joi error details
  * @returns problem json details object
  */
-const formatValidations = (errorsDetails) => {
-  const errors = {}
-  errors['invalid-params'] = errorsDetails.map((item) => {
-    return {
+const formatValidations = (
+  errorsDetails: ValidationErrorItem[],
+): { [key: string]: unknown } => {
+  return {
+    'invalid-params': errorsDetails.map((item) => ({
       name: item.path[0],
       reason: item.message.replace('/', ''),
-    }
-  })
-
-  return errors
+    })),
+  }
 }
 
 export { formatValidations }
