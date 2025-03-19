9921472e3a31de944329cd5120be7af339b448d7
Zayez <zayezuno@protonmail.com>
2025-03-19 19:32:07 -0300

    Typing fs-helper

--- server/src/helpers/fs-helper.ts
+++ server/src/helpers/fs-helper.ts
@@ -5,13 +5,13 @@ import config from '../config/config'
 
 const { isProd, isDev } = config
 
-const existsFile = async (file) =>
+const existsFile = async (filepath: string) =>
   await new Promise((resolve, reject) => {
-    const fileExists = fsSync.existsSync(file)
+    const fileExists = fsSync.existsSync(filepath)
     resolve(fileExists)
   })
 
-const deleteFile = async (filepath) => {
+const deleteFile = async (filepath: string) => {
   const filedir = isDev || isProd ? 'public' : 'tests/data'
   const file = path.join(filedir, filepath)
   try {
