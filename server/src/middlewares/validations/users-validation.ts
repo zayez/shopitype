dbd742fd0b3956a1fbfb10e10073f5926f6d9390
Zayez <zayezuno@protonmail.com>
2025-03-19 21:31:08 -0300

    Typing validation middlewares

--- server/src/middlewares/validations/users-validation.ts
+++ server/src/middlewares/validations/users-validation.ts
@@ -1,9 +1,10 @@
 import { validateBody } from '../request-validators'
 import { Create, Update } from '../schemas/users-schemas'
+import Koa from 'koa'
 
-const isValidCreate = async (ctx, next) =>
+const isValidCreate = async (ctx: Koa.Context, next: Koa.Next) =>
   await validateBody({ ctx, next }, Create)
-const isValidUpdate = async (ctx, next) =>
+const isValidUpdate = async (ctx: Koa.Context, next: Koa.Next) =>
   await validateBody({ ctx, next }, Update)
 
 export { isValidCreate, isValidUpdate }
